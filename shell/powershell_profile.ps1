1c80272b13cc1f5b6917be343b5683d2df30764b
yst_ <erik.leimert@web.de>
2025-03-31 20:58:27 +0200

    rename exst to nt

--- shell/powershell_profile.ps1
+++ shell/powershell_profile.ps1
@@ -1,7 +1,7 @@
 $env:STARSHIP_CONFIG = Join-Path $PSScriptRoot "shared/starship.toml"
 Invoke-Expression (&starship init powershell)
 
-$env:PATH += Join-Path $PSScriptRoot "shared/exst/target/release/" 
+$env:PATH += Join-Path $PSScriptRoot "shared/nt/target/release/" 
 
 if ((Get-Content $PROFILE) -notcontains $MyInvocation.MyCommand.Definition) {
     Add-Content $PROFILE $MyInvocation.MyCommand.Definition
